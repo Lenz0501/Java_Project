ecb3600f734b20cc687a119bb077614a4dd185fa
User <User@LAPTOP-039V192B>
2025-05-11 23:56:50 +0800

    建立User類別 功能：ID、密碼驗證、存款、提款、餘額查詢

---
+++ src/atm/User.java
@@ -0,0 +1,43 @@
+package atm;
+
+public class User {  //帳戶資訊
+	private String id;
+	private String username;
+	private String password;
+	private int balance;
+	
+	public User(String id, String username, String password, int balance) {  //初始值
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.balance = balance;
+	}
+	
+	public String getId() {  //取得id
+		return id;
+	}
+	
+	public boolean checkPassword(String inputPW) {  //檢查密碼
+		return password.equals(inputPW);
+	}
+	
+	public void deposit(int amount) {  //存款
+		balance += amount;
+	}
+	
+	public boolean withdraw(int amount) {  //提款
+		if (balance >= amount) {
+			balance -= amount;
+			return true;
+		}
+		return false;
+	}
+	
+	public int getBalance() {  //取得餘額
+		return balance;
+	}
+	
+	public String showInfo() {  //顯示帳戶資訊
+		return "帳號：" + id + "\n使用者：" + username + "\n餘額" + balance + " 元";
+	}
+}
